8cd8fba540ab69ca5f2387e9291d124b3da64cb0
kasp608b <54952896+kasp608b@users.noreply.github.com>
2023-06-02 16:09:19 +0200

    Created the Dto's

---
+++ DlsExamProject/Common/AuthorDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public class AuthorDto
+    {
+        public string? _id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<BookDto> Books { get; set; }
+    }
+}
---
+++ DlsExamProject/Common/BookDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Common
+{
+    public class BookDto
+    {
+        public string? _id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Description { get; set; }
+
+        public string Authorid { get; set; }
+
+        public AuthorDto Author { get; set; }
+
+    }
+}
