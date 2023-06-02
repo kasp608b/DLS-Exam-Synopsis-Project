2bf3e7898e336965e291e0a8d7950b3ec649c3c6
kasp608b <54952896+kasp608b@users.noreply.github.com>
2023-06-02 17:23:28 +0200

    Changed the kubernetes yml

---
+++ DlsExamProject/BooksApi/Models/BookDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace BooksApi.Models
+{
+    public class BookDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BookCollectionName { get; set; } = null!;
+    }
+}
