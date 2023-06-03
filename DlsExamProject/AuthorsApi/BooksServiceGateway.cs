1a36635c3687294baedfc1c2096f18998da94d9c
scupak <antonibujko@hotmail.com>
2023-06-03 19:32:06 +0200

    Booksservice gateway

--- DlsExamProject/AuthorsApi/BooksServiceGateway.cs
+++ DlsExamProject/AuthorsApi/BooksServiceGateway.cs
@@ -1,6 +1,23 @@
-﻿namespace AuthorsApi
+﻿using Common;
+using RestSharp;
+
+namespace AuthorsApi
 {
     public class BooksServiceGateway
     {
+        string AuthorServiceBaseUrl;
+
+        public BooksServiceGateway(string baseUrl)
+        {
+            AuthorServiceBaseUrl = baseUrl;
+        }
+
+        public Task<List<BookDto>?> GetBooksByAuthor(string AuthorId)
+        {
+            RestClient c = new RestClient(AuthorServiceBaseUrl);
+            var request = new RestRequest($"GetbooksByAuthor/{AuthorId}");
+            return c.GetAsync<List<BookDto>>(request);
+
+        }
     }
 }
--- DlsExamProject/AuthorsApi/Program.cs
+++ DlsExamProject/AuthorsApi/Program.cs
@@ -5,6 +5,7 @@ using Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string bookServiceBaseUrl = "http://booksapi/api/Books";
 // Add services to the container.
 builder.Services.Configure<AuthorDatabaseSettings>(
     builder.Configuration.GetSection("AuthorDatabase"));
@@ -12,6 +13,9 @@ builder.Services.Configure<AuthorDatabaseSettings>(
 builder.Services.AddSingleton<AuthorDatabaseService>();
 
 builder.Services.AddSingleton<IConverter<Author, AuthorDto>, AuthorConverter>();
+
+builder.Services.AddSingleton(new
+    BooksServiceGateway(bookServiceBaseUrl));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
